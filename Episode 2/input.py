30f0a52352873a9295e330dbe8c813d3d1b4503a
Max Oort <oortmax@gmail.com>
2021-12-25 21:31:16 -0500

    first commit

---
+++ Episode 2/input.py
@@ -0,0 +1,8 @@
+
+number1 = input("Enter a first number > ")
+number2 = input("Enter a second number > ")
+
+number1 = int(number1)
+number2 = int(number2)
+
+print(number1 + number2)
---
+++ Episode 3/main.py
@@ -0,0 +1,11 @@
+
+string = 'Hello world!'
+integer = 80
+float = 80.70
+
+number1 = 99.80
+number2 = 30.45
+
+number3 = number1 + number2
+
+print(number3)
