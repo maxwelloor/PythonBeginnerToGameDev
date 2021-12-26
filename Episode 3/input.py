4cd7f190d05ba995b394cc161520ceb819e67bf0
Max Oort <oortmax@gmail.com>
2021-12-26 00:13:49 -0500

    Episode 3

--- Episode 3/input.py
+++ Episode 3/input.py
@@ -5,4 +5,25 @@ number2 = input("Enter a second number > ")
 number1 = int(number1)
 number2 = int(number2)
 
-print(number1 + number2)
+operation = input("Enter a sign > ")
+
+if operation == "+":
+    print(number1 + number2)
+elif operation == "-":
+    print(number1 - number2)
+elif operation == "/":
+    print(number1 / number2)
+elif operation == "*":
+    print(number1 * number2)
+else:
+    print("This is not a valid operation")
+
+
+
+
+
+
+
+
+
+
